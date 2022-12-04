7cb147b3c938a11d41959a6cb0ea296eb99f37fd
MRIDUL ARORA <aroramridul18@gmail.com>
2022-12-05 05:19:53 +0530

    chore: style and logic done

---
+++ main.js
@@ -0,0 +1,64 @@
+//common text fields
+const outputText = document.querySelector("#outputText");
+const outputHint = document.querySelector("#outputHint");
+
+//common functions 
+function showText(text) {
+    outputText.innerText = text;
+}
+
+function showHint(text) {
+    outputHint.style.display = "block";
+    outputHint.innerText = text;
+}
+
+function hideHint() {
+    outputHint.style.display = "none";
+}
+
+// Profit & Loss - Stocks
+const intitalPrice = document.querySelector("#intprice");
+const quantityOfStocks = document.querySelector("#qnty");
+const currentPrice = document.querySelector("#curprice");
+const btnProfitnLoss = document.querySelector("#btn-profitnloss");
+
+function clickHandler() {
+    var priceAtWhichBought = Number(intitalPrice.value);
+    var priceAtWhichSold = Number(currentPrice.value);
+    var quantityOfStock = Number(quantityOfStocks.value);
+    var profitnloss = 0,
+        totalPnL = 0,
+        pnlPerc = 0,
+        flag = 0;
+
+    console.log(priceAtWhichSold);
+    console.log(priceAtWhichBought);
+
+    if (priceAtWhichSold === priceAtWhichBought) {
+        flag = 0;
+        console.log(flag);
+        profitnloss = 0;
+        totalPnL = 0;
+        pnlPerc = 0;
+        showText("No profit, No loss.");
+        showHint("Actually, you lost money on transaction fees, TTS, etc. ");
+    } else if (priceAtWhichSold < priceAtWhichBought) {
+        flag = -1;
+        console.log(flag);
+        profitnloss = priceAtWhichBought - priceAtWhichSold;
+        totalPnL = profitnloss * quantityOfStock;
+        pnlPerc = profitnloss * 100 / priceAtWhichBought;
+        showText("Ohhoo..! Loss: Rs." + totalPnL + ", and your Loss% is: " + pnlPerc + "%.");
+        hideHint();
+    } else {
+        flag = 1;
+        console.log(flag);
+        profitnloss = priceAtWhichSold - priceAtWhichBought;
+        totalPnL = profitnloss * quantityOfStock;
+        pnlPerc = profitnloss * 100 / priceAtWhichBought;
+        showText("Congrats! Profit: Rs." + totalPnL + ", and your Profit% is: " + pnlPerc + "%.");
+        hideHint();
+    }
+}
+
+btnProfitnLoss.addEventListener('click', clickHandler);
